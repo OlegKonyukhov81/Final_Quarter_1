0ef654fd768e75ec75008abcf4f918b6b1c28c59
*Oleg_Konyukhov_81* <*Konykhov1@mail.ru*>
2023-04-04 03:59:19 +0400

    Added_PrintText

--- Final_Exam/Program.cs
+++ Final_Exam/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк,
+// длина которых равна или меньше 3 символов.
+
+string[] text = {"хвост","за","хвост","глаз","за","глаз","никуда","ты","не","уйдешь","от","нас"};
+
+void PrintText(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"'");
+        Console.Write($"{array[i]}");
+        Console.Write($"'");
+        if (i != array.Length - 1) Console.Write($", ");
+    }
+}
+
+Console.Write($"[");
+PrintText(text);
+Console.Write($"]");
